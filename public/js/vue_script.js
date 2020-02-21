bee43c83dc5553701574b1e832eb30520b604fd1
daba6626 <danno-082011@hotmail.com>
2020-02-21 15:16:54 +0100

    New tag 5-2 done.

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,22 +1,25 @@
+'use strict';
+const socket = io();
+
 /*document.getElementById("myID").innerHTML = "Välj en burgare";
-const v = new Vue({
+  const v = new Vue({
   el: '#yID',
   data: {
-    arbitraryVariableName: 'Välj en burgare'
+  arbitraryVariableName: 'Välj en burgare'
   }
-})*/
+  })*/
 
 
 
 //let menu = [TheMightyBurger, TheOriginalBurger, TheChickenator, ITBurger, DVBurger];
 
 const vm = new Vue({
-  el: '#myID',
-  data: {
-  	
-  	  food: food
-        	
-  }
+    el: '#myID',
+    data: {
+  	    
+  	    food: food
+        
+    }
 })
 
 const btn = new Vue({
@@ -28,13 +31,30 @@ const btn = new Vue({
         house: "",
         pm:"Cash",
         gender: "Female",
-        output: ""
+        output: "",
+        orders: {},
         
         
     },
+    created: function() {
+        /* When the page is loaded, get the current orders stored on the server.
+         * (the server's code is in app.js) */
+        socket.on('initialize', function(data) {
+            this.orders = data.orders;
+        }.bind(this));
+
+        /* Whenever an addOrder is emitted by a client (every open map.html is
+         * a client), the server responds with a currentQueue message (this is
+         * defined in app.js). The message's data payload is the entire updated
+         * order object. Here we define what the client should do with it.
+         * Spoiler: We replace the current local order object with the new one. */
+        socket.on('currentQueue', function(data) {
+            this.orders = data.orders;
+        }.bind(this));
+    },
     methods:{
         clickedBtn: function(){
-           
+            
             this.output = this.fullname+ " " + this.email + " " + this.street + " " + this.house + " " + this.pm + " " + this.gender
 
             let burger = document.getElementsByName('checkBurger');
@@ -45,7 +65,36 @@ const btn = new Vue({
                     this.output = this.output + " " +  burger[i].value;
                 }
             }
-        }
+        },
+
+        getNext: function() {
+            /* This function returns the next available key (order number) in
+             * the orders object, it works under the assumptions that all keys
+             * are integers. */
+            let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
+                return Math.max(last, next);
+            }, 0);
+            return lastOrder + 1;
+        },
+        addOrder: function(event) {
+            /* When you click in the map, a click event object is sent as parameter
+             * to the function designated in v-on:click (i.e. this one).
+             * The click event object contains among other things different
+             * coordinates that we need when calculating where in the map the click
+             * actually happened. */
+            let offset = {
+                x: event.currentTarget.getBoundingClientRect().left,
+                y: event.currentTarget.getBoundingClientRect().top,
+            };
+            socket.emit('addOrder', {
+                orderId: this.getNext(),
+                details: {
+                    x: event.clientX - 10 - offset.x,
+                    y: event.clientY - 10 - offset.y,
+                },
+                orderItems: ['Beans', 'Curry'],
+            });
+        },
     }
 })
 
@@ -55,25 +104,25 @@ const btn = new Vue({
 /*const vm = new Vue({ //TODO Change CONST VM TO CONST X ?
   el: '#myID',
   data: {
-  	B1: 'The Mighty Burger',
-    B2: 'The Original Burger',
-    B3: 'The Chickenator',
-    B4: 'ITBurger',
-    B5: 'DVBurger'
+  B1: 'The Mighty Burger',
+  B2: 'The Original Burger',
+  B3: 'The Chickenator',
+  B4: 'ITBurger',
+  B5: 'DVBurger'
   }
   })*/
 
 
 /*
-<div id="myID">
-<h1>
-Välj en burgare
-</h1>
+  <div id="myID">
+  <h1>
+  Välj en burgare
+  </h1>
 
   <li v-for="burger in menu">
   {{burger.info()}}
   </li>
- 
- </div>  
+  
+  </div>  
 
 */
